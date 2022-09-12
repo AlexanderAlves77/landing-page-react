8f48bacacdb23df90fa02b18579ecb89dc511fd8
Alexander Alves <vendas77.rj@gmail.com>
2022-09-12 12:47:54 -0300

    Criando o componente GoTop

---
+++ src/components/GoTop/GoTop.test.jsx
@@ -0,0 +1,19 @@
+import { screen } from '@testing-library/react'
+import { GoTop } from '.'
+import { renderTheme } from '../../styles/render-theme'
+
+describe('<GoTop />', () => {
+    it('should render', () => {
+        renderTheme(<GoTop>Children</GoTop>);
+        expect(screen.getByRole('heading')).toBeInTheDocument()
+    })
+
+    it('should render a go to top button', () => {
+        const { container } = renderTheme(<GoTop />);
+        expect(screen.getByRole('link', { name: 'Go to top' })).toBeInTheDocument();
+        expect(screen.getByRole('link', { name: 'Go to top' })).toHaveAttribute(
+            'href', '#',
+        )
+        expect(container).toMatchInlineSnapshot()
+    })
+})
